a6fe86163a144888e2e4ef6bd1ad9055698b3dfa
AkmalShofy <114986742+AkmalShofy@users.noreply.github.com>
2022-10-25 20:02:57 +0800

    Tugas 4

--- PRAK304-2210817210023-MuhammadShofyAkmal.c
+++ PRAK304-2210817210023-MuhammadShofyAkmal.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main () {
-float a;
-printf ("Masukkan Bilangan Cacah : "); scanf ("%f", &a);
-if (a==0) {printf ("Nol");}
-else if (0<a<10) {printf ("Satuan");}
-else if (10<a<20) {printf ("Belasan");}
-else if (a>=100) {printf ("Anda Menginput Melebihi Limit Bilangan");}
-else {printf ("Puluhan");}
+int a;
+printf ("Masukkan Bilangan Cacah : "); scanf ("%d", &a);
+if      (a == 0)  
+{printf ("Nol");                                   }
+else if (a>0 & a<10)  
+{printf ("Satuan");                                }
+else if (a>10 & a<20)  
+{printf ("Belasan");                               }
+else if (a>=100 )  
+{printf ("Anda Menginput Melebihi Limit Bilangan");}
+else               
+{printf ("Puluhan");                               }
 }
